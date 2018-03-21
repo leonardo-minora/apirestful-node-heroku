603c5a1adefff420b4e43eb7abc7101d97c1fab6
L. A. Minora <leonardo.minora@gmail.com>
2018-03-21 14:04:53 -0300

    exemplo de URL retornando dados fantasmas

--- app/api.js
+++ app/api.js
@@ -7,5 +7,18 @@ app.get('/', function (req, res) {
     version: "0.0.1"
   });
 })
- 
+
+// /apelido/tasks (GET, POST, PUT, DELETE, HEAD)
+// /apelido/tasks/id (GET, POST, PUT, DELETE, HEAD)
+
+app.get('/minora/tasks', (requisicao, resposta) => {
+  resposta.json({
+    user: "minora",
+    tasks: [
+      {id: "123", title: "Teach API Restful"},
+      {id: "321", title: "Distributed systems"}
+    ]
+  });
+})
+
 app.listen(process.env.PORT || 3000);
