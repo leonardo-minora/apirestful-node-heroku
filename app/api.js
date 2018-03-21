182a176b0d50ebc1dde42b8e3b4312d419db4da0
Tiago Lisboa <tiago.caio.ol@gmail.com>
2018-03-21 13:29:35 -0300

    1a versão da api

---
+++ app/api.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var app = express()
+ 
+app.get('/', function (req, res) {
+  res.json({
+    name: "API Restful example",
+    version: "0.0.1"
+  });
+})
+ 
+app.listen(3000);
